6ad015f1b53a0ca3e1d383da1fa23326a7e64585
Axisco <darthxisco@gmail.com>
2018-03-12 21:29:03 +0100

    users able to persist

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,45 @@
-Hi
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Users</h1>
+
+    <table class="table">
+        <thead>
+          <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Active</th>
+              <th>Created</th>
+              <th>Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        @if($users)
+
+            @foreach($users as $user)
+
+              <tr>
+                  <td>{{$user->id}}</td>
+                  <td>{{$user->name}}</td>
+                  <td>{{$user->email}}</td>
+                  @if (isset($user->role->name) == false)
+                    <td>User has no role</td>
+
+                  @else
+                    <td>{{$user->role->name}}</td>
+                  @endif
+                  <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
+                  <td>{{$user->created_at->diffForHumans()}}</td>
+                  <td>{{$user->updated_at->diffForHumans()}}</td>
+              </tr>
+
+          @endforeach
+
+        @endif
+
+        </tbody>
+    </table>
+
+@endsection
